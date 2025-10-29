c9bcc5194beca1193fb1f7ff4797e52d702bafe6
Emil Andersson <eander0105@gmail.com>
2025-10-29 18:28:07 +0100

    commented out some gpt shenanigans

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,7 +4,7 @@ vim.g.maplocalleader = "\\"
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-map("n", "<leader>w", ":w<CR>", opts)
-map("n", "<leader>q", ":q<CR>", opts)
-map("n", "<leader>h", ":nohlsearch<CR>", opts)
-map("n", "C-e", ":Explore", opts)
+-- Suggested by gipity, might add later
+-- map("n", "<leader>w", ":w<CR>", opts)
+-- map("n", "<leader>q", ":q<CR>", opts)
+-- map("n", "<leader>h", ":nohlsearch<CR>", opts)
