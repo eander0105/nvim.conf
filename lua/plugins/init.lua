69ea2c2041505fcfa8fe756ce01c4819be5bc6f0
Emil Andersson <eander0105@gmail.com>
2025-10-30 11:25:24 +0100

    harpoon plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,15 @@
 return {
+  -- UI, theme etc
   { import = "plugins.ui" },
-  { import = "plugins.files" },
-  { import = "plugins.coding" },
+
+  -- Tools
   { import = "plugins.git" },
   { import = "plugins.ai" },
+
+  -- Files and navigation management
+  { import = "plugins.files" },
+  { import = "plugins.harpoon" },
+
+  -- Coding, LSP, autocomplete etc
+  { import = "plugins.coding" },
 }
