fdb0466cd1ad137bc385b7d6817e65df01d9ab6b
Emil Andersson <eander0105@gmail.com>
2025-10-30 15:20:25 +0100

    comments

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,11 +2,11 @@ return {
   -- UI, theme etc
   -- TODO: split plugins.ui into their own files
   { import = "plugins.ui" },
-  { import = "plugins.todohl" },
+  { import = "plugins.todohl" }, -- Todo highlight
 
   -- Tools
   { import = "plugins.git" },
-  { import = "plugins.ai" },
+  { import = "plugins.ai" }, -- Collection of AI features
 
   -- Files and navigation management
   -- TODO: split plugins.files into their own files
